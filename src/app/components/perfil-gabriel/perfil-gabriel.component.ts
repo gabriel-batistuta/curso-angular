b0f63ed6d610618321b59d9e5ebd065ed2f19f97
Gabriel Batistuta <batistutag190@gmail.com>
2022-08-02 16:18:08 -0300

    exercicio de troca de perfis utilizando interpolação de dados e condicionais

--- src/app/components/perfil-gabriel/perfil-gabriel.component.ts
+++ src/app/components/perfil-gabriel/perfil-gabriel.component.ts
@@ -7,12 +7,33 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class PerfilGabrielComponent implements OnInit {
 
+  show: boolean = false;
+  tentativa2: boolean = false;
   @Input() dados_do_estudante = {
-    name : 'Gabriel Batistuta',
-    function: 'Estudante',
-    age: 18,
-    turno: 'Integral',
-    curso: 'Ciência da Computação'
+    name : '',
+    function: '',
+    age: 0,
+    turno: '',
+    curso: ''
+  }
+  return() {
+      this.show = !this.show;
+  }
+  showPerfilGabriel() {
+    this.dados_do_estudante.name = 'Gabriel Batistuta';
+    this.dados_do_estudante.function = 'Estudante';
+    this.dados_do_estudante.age = 18;
+    this.dados_do_estudante.turno = 'Integral';
+    this.dados_do_estudante.curso = 'Ciência da Computação';
+    this.show = true;
+  }
+  showPerfilJoao() {
+    this.dados_do_estudante.name = 'João da Silva Filho';
+    this.dados_do_estudante.function = 'Professor';
+    this.dados_do_estudante.age = 26;
+    this.dados_do_estudante.turno = 'Noturno';
+    this.dados_do_estudante.curso = 'Matemática';
+    this.show = true;
   }
   constructor() { }
 
