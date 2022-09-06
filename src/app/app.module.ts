658c3b2fd7ca8e62330d79382b8237d2a71f4998
Gabriel Batistuta <batistutag190@gmail.com>
2022-09-06 15:32:10 -0300

    exemplos de forma de utilizar o Angular implementadas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http'
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
 import { ParentDataComponent } from './components/parent-data/parent-data.component';
@@ -11,6 +12,10 @@ import { IfRenderComponent } from './components/if-render/if-render.component';
 import { EventosComponent } from './components/eventos/eventos.component';
 import { EmitterComponent } from './components/emitter/emitter.component';
 import { ChangeNumberComponent } from './components/change-number/change-number.component';
+import { ListRenderComponent } from './components/list-render/list-render.component';
+import { PipesComponent } from './components/pipes/pipes.component';
+import { TwoWayBindingComponent } from './components/two-way-binding/two-way-binding.component';
+import { ItemDetailComponent } from './components/item-detail/item-detail.component';
 
 @NgModule({
   declarations: [
@@ -22,11 +27,18 @@ import { ChangeNumberComponent } from './components/change-number/change-number.
     IfRenderComponent,
     EventosComponent,
     EmitterComponent,
-    ChangeNumberComponent
+    ChangeNumberComponent,
+    ListRenderComponent,
+    PipesComponent,
+    TwoWayBindingComponent,
+    ItemDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AppRoutingModule, //new
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/list-render/list-render.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Animal } from 'src/app/Animal';
+
+import { ListService } from 'src/app/services/list.service'; // new 
+
+@Component({
+  selector: 'app-list-render',
+  templateUrl: './list-render.component.html',
+  styleUrls: ['./list-render.component.css']
+})
+export class ListRenderComponent implements OnInit {
+  animals: Animal[] = [
+    { name: "Turca", type: "Dog", age: 4 },
+    { name: "Tom", type: "Cat", age: 10 },
+    { name: "Frida", type: "Dog", age: 5 },
+    { name: "Bob", type: "Horse", age: 1 },
+  ];
+
+  animal: Animal = {
+    name: 'Teste',
+    type: 'Alguma coisa',
+    age: 10,
+  }
+
+  animalDetails = '';
+
+  constructor(private listService: ListService) { 
+    this.getAnimals()
+   }
+
+  ngOnInit(): void {
+  }
+  
+  showAge(animal: Animal) :void {
+    this.animalDetails = `O pet ${animal.name} tem ${animal.age} anos!`;
+  }
+
+  removeAnimal(animal: Animal) {
+    console.log('Removendo animal...')
+    this.animals = this.listService.remove(this.animals, animal);
+  } 
+}
+
+  getAnimals(): void {
+    this.listService.getAll().subscribe((animals) => (this.animals = animals));
+  }
